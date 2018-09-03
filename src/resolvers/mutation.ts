f15ba37432d8d5d155c4c65d418f8181afa8a605
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2018-09-03 20:32:04 +0300

    Complete remaining mutation resolvers

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,4 +1,6 @@
 
+const FormData = require('form-data');
+
 export function registerUser(obj, { email, password, typ }, { dataloader }) {
   return dataloader.load(['/api/auth/register', {
     method: 'POST',
@@ -16,5 +18,55 @@ export function loginUser(obj, { email, password }, { dataloader }) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ email, password })
+  }])
+}
+
+export function verifyEmail(obj, { code }, { dataloader }) {
+  return dataloader.load(['/api/auth/verify?' + code, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }])
+}
+
+export function requestResetPassword(obj, { email }, { dataloader }) {
+  let form = new FormData();
+  form.append('email', email);
+  return dataloader.load(['/api/auth/password/send-link', {
+    method: 'POST',
+    body: form
+  }])
+}
+
+export function resetPassword(obj, { code, password }, { dataloader }) {
+  let form = new FormData();
+  form.append('code', code);
+  form.append('password', password);
+  return dataloader.load(['/api/auth/password/reset', {
+    method: 'POST',
+    body: form
+  }])
+}
+
+export function changePassword(obj, { access_token, password }, { dataloader }) {
+  let form = new FormData();
+  form.append('password', password);
+  return dataloader.load(['/api/auth/password/change', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + access_token,
+      'Content-Type': 'text/html; charset=UTF-8'
+    },
+    body: form
+  }])
+}
+
+export function uploadAvatar(obj, { access_token, avatar }, { dataloader }) {
+  return dataloader.load(['/api/user/profile/avatar', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    },
+    body: avatar
   }])
 }
